2eb4429c90f627a8fbe01388857452ae9e9d6d58
Naroah <yanghaoran13228@gmail.com>
2018-10-25 17:12:27 +0800

    organize code in lab/views.py

--- lab/views.py
+++ lab/views.py
@@ -1,33 +1,47 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from data_lab import settings
 
-
-# 使用测试数据注释第二行，使用数据库数据注释第一行
-from .TestData import TestData
-# from lab.mongo_db import *
+if settings.DEBUG is True:
+    from .TestData import TestData
+else:
+    from lab.mongo_db import database
 
 
 def index(request, keyword='rivaroxaban', page=1):
+    """all data will be returned by search, \n
+    if not given, there's a default keyword.
+
+    Arguments:
+        request {} -- none
+
+    Keyword Arguments:
+        keyword {str} -- [name of gene or compound] (default: {'rivaroxaban'})
+        page {int} -- [for pagination] (default: {1})
+
+    Returns:
+        [HttpResponse] -- none
+    """
+
     if request.method == 'POST':
         keyword = request.POST.get('keyword', 'rivaroxaban')
-        page = request.POST.get('page', 1)
+        page = int(request.POST.get('page', 1))
     test_data_set = TestData()
 
-    # 实际用数据
-    # data = query_gene(keyword)
-    # if not data:
-    #     data = query_compound(keyword)
-    # -----------------------
+    if settings.DEBUG is True:
+        # 测试用数据
+        data = test_data_set.get_test_data(keyword)
+    else:
+        # 实际用数据
+        data = search(keyword, True)
+        database.close()
 
-    # 测试用数据
-    data = test_data_set.get_test_data(keyword)
-    # -----------------------
-    data = sorted(data, key=lambda x: x['cite_num'], reverse=True)
     paginator = Paginator(data, 10)
-    if int(page) < 1:
+    print(type(page))
+    if page < 1:
         page = 1
-    elif int(page) > paginator.num_pages:
+    elif page > paginator.num_pages:
         page = paginator.num_pages
     context = {
         'data': paginator.get_page(page),
@@ -38,5 +52,9 @@ def index(request, keyword='rivaroxaban', page=1):
     return render(request, 'lab/semanticwb.html', context=context)
 
 
-def search(request, keyword):
-    pass
+def search(keyword: str, set_sort: bool = True) -> list:
+    data = database.query_gene(keyword)
+    if not data:
+        data = database.query_compound(keyword)
+    database.close()
+    return sorted(data, key=lambda x: x['cite_num'], reverse=True)
